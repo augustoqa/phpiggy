a29ba54873a223c604a307b512548d59b8a43108
Cesar Quisquinay <augustoacual@gmail.com>
2024-06-17 21:47:29 -0600

    Rendering a Template

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController
 
     public function home()
     {
-        dd($this->view);
-        echo 'home page';
+        $secret = 'fjkdsfjkd';
+        $this->view->render('/index.php');
     }
 }

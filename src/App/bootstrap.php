5152ee4ab9437bda8ec90ca2d69223eb5b455e08
Cesar Quisquinay <augustoacual@gmail.com>
2024-07-19 19:25:18 -0600

    Create the AboutController and the about view

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,9 +6,11 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use Framework\App;
 use App\Controllers\HomeController;
+use App\Controllers\AboutController;
 
 $app = new App();
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
